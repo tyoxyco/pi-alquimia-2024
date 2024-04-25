c734e47f838458e615797c96ffd65649fcd9598f
Jorge Machado <135711377+tyoxyco@users.noreply.github.com>
2024-04-25 19:47:44 -0300

    Update script.js
    Script para janela de validação

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-alert('Olá visitante!')
-confirm("Você é maior de idade?")
+function verificarIdade() {
+    var idade = document.getElementById('idadeInput');
+        var idade = idadeInput.value;    
+ 
+    if (idade >=18) {
+        
+        var dialogBox = document.querySelector('.dialog-box');
+            dialogBox.computedStyleMap.display = "none";
+        
+        window.location.href = "https://alquimiacervejaria.com.br/index.html";   
+
+        alert("Seja Bem Vindo");
+    }   
+        else {
+        window.location.href = "https://google.com";
+    }
+}
